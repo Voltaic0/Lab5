da9231ccc2a69fe7a4fc456e699f58f6f635e59b
adecandi <adecandi@purdue.edu>
2020-04-26 18:35:06 -0400

    files.h

--- flat/include/os/files.h
+++ flat/include/os/files.h
@@ -4,7 +4,19 @@
 #include "dfs.h"
 #include "files_shared.h"
 
-//#define FILE_MAX_OPEN_FILES 15
+#define FILE_MAX_OPEN_FILES 15
+#define FILE_MODE_R 1
+#define FILE_MODE_W 2
+#define FILE_MODE_RW 3
+
+void FileModuleInit();
+uint32 FileOpen(char* filename, int mode);
+int FileClose(); //File functions need arguments still
+int FileRead(); 
+int FileWrite();
+int FileSeek();
+int FileDelete();
+uint32 FileDescFilenameExists(char* filename);
 
 
 
