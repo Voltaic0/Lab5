5353a96e1338d3bcaff1b16fdd13da697dcac314
adecandi <adecandi@purdue.edu>
2020-04-26 19:13:45 -0400

    dfs.h

--- flat/include/os/dfs.h
+++ flat/include/os/dfs.h
@@ -3,6 +3,26 @@
 
 #include "dfs_shared.h"
 
+void DfsModuleInit();
+void DfsInvalidate();
+
+//Non-inode Functions (not sure about arguments yet)
+int DfsOpenFilesSystem();
+int DfsCloseFileSystem();
+uint32 DfsAllocateBlock();
+int DfsFreeBlock();
+int DfsReadBlock();
+int DfsWriteBlock();
+
+//Inode Functions (not sure about arguments yet)
+uint32 DfsInodeFilenameExists();
+uint32 DfsInodeOpen();
+int DfsInodeDelete();
+int DfsInodeReadBytes();
+int DfsInodeWriteBytes();
+uint32 DfsInodeFilesize();
+uint32 DfsInodeAllocateVirtualBlock();
+uint32 DfsInodeTranslateVirtualToFilesys();
 
 
 #endif
